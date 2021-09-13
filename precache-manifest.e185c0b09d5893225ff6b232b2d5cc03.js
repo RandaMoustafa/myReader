eef35881d25db1a6aa91f16b7e17ec8ebeb02b4c
RandaMoustafa <fairystory93@gmail.com>
2021-09-13 22:14:33 +0200

    Updates

---
+++ precache-manifest.e185c0b09d5893225ff6b232b2d5cc03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1397f2d304de25f2c7d3",
+    "url": "/myReader/static/css/main.3078f0e6.chunk.css"
+  },
+  {
+    "revision": "1397f2d304de25f2c7d3",
+    "url": "/myReader/static/js/main.1397f2d3.chunk.js"
+  },
+  {
+    "revision": "4f3e03617884a0b9477c",
+    "url": "/myReader/static/js/1.4f3e0361.chunk.js"
+  },
+  {
+    "revision": "1322bbfa43a552512b54",
+    "url": "/myReader/static/js/runtime~main.1322bbfa.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myReader/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myReader/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myReader/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "4c6a990829b1a0f28662a7789db6ca2f",
+    "url": "/myReader/index.html"
+  }
+];
